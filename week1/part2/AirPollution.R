4c7d40619f74e9e9018d1806e1422d3f73b9b21e
LeeChiuLan <magicq2005@yahoo.com.tw>
2019-07-12 15:23:05 +0800

    updating ...

--- week1/part2/AirPollution.R
+++ week1/part2/AirPollution.R
@@ -1,21 +1,34 @@
 library(dplyr)
-datafile <- "/Users/linda/Desktop/CS+X-108/20190711/convertcsv.csv"
-#sitefile <- "/Users/linda/Desktop/CS+X-108/20190711/空氣品質監測站.csv"
+restaurantfile <- "/Users/linda/Desktop/CS+X-108/20190711/convertcsv.csv"
+#sitefile <- "/Users/linda/Desktop/CS+X-108/20190711/空氣品質監測站Eng.csv"
 
 
-data<-read.csv(datafile, header=T, sep=",")
-dim(data)
-
+restaurantdata_raw<-read.csv(restaurantfile, header=T, sep="," , na.strings = c("", "NA"))
+#restaurantdata_raw = restaurantdata_raw %>% na.omit()   # remove emty rows
+dim(restaurantdata_raw)
+head(restaurantdata_raw)
 #sitedata<-read.csv(sitefile, header=T, sep=",")
-sitedata<-read.csv("/Users/linda/Desktop/CS+X-108/20190711/空氣品質監測站.csv", header=T, sep=",")
-dim(sitedata)
+sitedata_raw<-read.csv("/Users/linda/Desktop/CS+X-108/20190711/空氣品質監測站Eng.csv", header=T, sep=",", na.strings = c("", "NA"))
+sitedata_raw= sitedata_raw %>% na.omit()   # remove emty rows
+dim(sitedata_raw)
+head(sitedata_raw)
+
+sitedata <- select(sitedata_raw, Name, Longitude,Latitude)
+head(sitedata)
+sitedata$Name <- paste(sitedata$Name,"Site", sep="_")   #append "Site" to each $Name
 head(sitedata)
 
+names(sitedata)[names(sitedata) == "Longitude"] <- "Px"
+names(sitedata)[names(sitedata) == "Latitude"] <- "Py"
+Description=""
+sitedata = cbind(sitedata, Description)
+head(sitedata)
+dim(sitedata)
 #---------------
-locationDate <- select(data, Name, Zipcode, Px, Py)
-head(locationDate)
+restaurantdata <- select(restaurantdata_raw, Name, Px, Py, Description)
+head(restaurantdata)
+dim(restaurantdata)
 
-nrow(locationDate)
-
-EatPollution = rbind(data, sitedata)
+data = rbind(restaurantdata, sitedata)
 library(kableExtra)
+
