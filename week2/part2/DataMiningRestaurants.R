147ff6cbfe6ca75119fae502e25e219ae6f8e996
LeeChiuLan <magicq2005@yahoo.com.tw>
2019-07-22 00:21:45 +0800

    update DataMiningRestaurants.R

--- week2/part2/DataMiningRestaurants.R
+++ week2/part2/DataMiningRestaurants.R
@@ -1,7 +1,7 @@
 library(dplyr)
+library(NLP)
 library(xml2)
 library(tmcn)
-library(NLP)
 library(jiebaRD)
 library(jiebaR)
 library(RColorBrewer)
@@ -82,9 +82,111 @@ getdoc(dff,des_dir)
 
 txt<-system.file("texts","txt",package = 'tm')
 docs<-Corpus(DirSource(txt,encoding = "UTF-8"))
-writeCorpus(docs, path = "./DATA", filenames = NULL)
-
+#writeCorpus(docs, path = "./DATA", filenames = NULL)
 
 
 ### 建立文本資料結構與基本文字清洗
+d.corpus <- Corpus( DirSource("./DATA") )
+d.corpus <- tm_map(d.corpus, removePunctuation)
+d.corpus <- tm_map(d.corpus, removeNumbers)
+d.corpus <- tm_map(d.corpus, function(word) {
+  gsub("[A-Za-z0-9]", "", word)
+})
 
+### 進行斷詞，並依照縣市建立文本矩陣 TermDocumentMatrix
+mixseg = worker()
+jieba_tokenizer = function(d)
+{
+  unlist( segment(d[[1]], mixseg) )
+}
+seg = lapply(d.corpus, jieba_tokenizer)
+
+count_token = function(d)
+{
+  as.data.frame(table(d))
+}
+tokens = lapply(seg, count_token)
+
+n = length(seg)
+TDM = tokens[[1]]
+colNames <- names(seg)
+colNames <- gsub(".txt", "", colNames)
+for( id in c(2:n) )
+{
+  TDM = merge(TDM, tokens[[id]], by="d", all = TRUE)
+  names(TDM) = c('d', colNames[1:id])
+}
+TDM[is.na(TDM)] <- 0
+library(knitr)
+kable(head(TDM))
+
+kable(tail(TDM))
+
+### 將已建好的 TDM 轉成 TF-IDF
+tf <- apply(as.matrix(TDM[,2:(n+1)]), 2, sum)
+
+library(Matrix)
+idfCal <- function(word_doc)
+{ 
+  log2( n / nnzero(word_doc) ) 
+}
+idf <- apply(as.matrix(TDM[,2:(n+1)]), 1, idfCal)
+
+doc.tfidf <- TDM
+
+tempY = matrix(rep(c(as.matrix(tf)), each = length(idf)), nrow = length(idf))
+tempX = matrix(rep(c(as.matrix(idf)), each = length(tf)), ncol = length(tf), byrow = TRUE)
+doc.tfidf[,2:(n+1)] <- (doc.tfidf[,2:(n+1)] / tempY) * tempX
+
+stopLine = rowSums(doc.tfidf[,2:(n+1)])
+delID = which(stopLine == 0)
+
+kable(head(doc.tfidf[delID,1]))
+kable(tail(doc.tfidf[delID,1]))
+
+TDM = TDM[-delID,]
+doc.tfidf = doc.tfidf[-delID,]
+
+### TF-IDF 文章取得的重要關鍵字
+TopWords = data.frame()
+for( id in c(1:n) )
+{
+  dayMax = order(doc.tfidf[,id+1], decreasing = TRUE)
+  showResult = t(as.data.frame(doc.tfidf[dayMax[1:10],1]))
+  TopWords = rbind(TopWords, showResult)
+}
+rownames(TopWords) = colnames(doc.tfidf)[2:(n+1)]
+TopWords = droplevels(TopWords)
+kable(TopWords)
+
+### TF-IDF Hours 文章取得的重要關鍵字 TDM merge 視覺化
+TDM$d = as.character(TDM$d)
+AllTop = as.data.frame( table(as.matrix(TopWords)) )
+AllTop = AllTop[order(AllTop$Freq, decreasing = TRUE),]
+
+kable(head(AllTop))
+
+### ggplot 
+TopNo = 6
+tempGraph = data.frame()
+for( t in c(1:TopNo) )
+{
+  word = matrix( rep(c(as.matrix(AllTop$Var1[t])), each = n), nrow = n )
+  temp = cbind( colnames(doc.tfidf)[2:(n+1)], t(TDM[which(TDM$d == AllTop$Var1[t]), 2:(n+1)]), word )
+  colnames(temp) = c("city", "freq", "words")
+  tempGraph = rbind(tempGraph, temp)
+  names(tempGraph) = c("city", "freq", "words")
+}
+
+library(ggplot2)
+
+library(varhandle)
+tempGraph$freq = unfactor(tempGraph$freq)
+ggplot(tempGraph, aes(city, freq)) + 
+  geom_point(aes(color = words, shape = words), size = 5) +
+  geom_line(aes(group = words, linetype = words))+
+  theme(text=element_text(family="黑體-繁 中黑", size=14))
+
+kable(tail(AllTop))
+
+### 
